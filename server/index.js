56b03b7ec8437e1bdfcdb725428f8f818c25f5a5
Himanshu Verma <vhimanshu0817@gmail.com>
2025-11-19 02:47:10 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 require('dotenv').config();
 const User =require('./models/userModel');
 const dbConnect = require('./config/dbConnection');
@@ -12,11 +13,11 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-app.use('/api/user', userRouter);
 
 app.get('/', (req, res) => {
     res.send('Server is working perfectly');
 });
+app.use('/api/user', userRouter);
 
 // Listen only when running locally
 if (process.env.NODE_ENV !== "production") {
