24678ed776b8fb5ca09680798a01b6ee4045e49f
Himanshu Verma <vhimanshu0817@gmail.com>
2025-11-19 01:58:16 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -12,6 +12,15 @@ app.get('/', (req, res) => {
     res.send('Server is working perfectly');
 });
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+// Listen only when running locally
+if (process.env.NODE_ENV !== "production") {
+    const PORT = process.env.PORT || 5000;
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+}
+// app.listen(PORT, (err)=>{
+//     if(!err){
+//         console.log(`Server is running on ${PORT}`);
+//     }
+// })
+
+module.exports = app;
