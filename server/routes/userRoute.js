ae3bdd92ce5903dc8d25437821516f5413becd66
Himanshu Verma <vhimanshu0817@gmail.com>
2025-11-19 22:19:07 +0530

    updating commit

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,13 +1,3 @@
-<<<<<<< HEAD
-const express = require('express');
-const { clerkWebHooks } = require('../controllers/userController');
-const userRouter = express.Router();
-
-userRouter.post('/webhooks', express.raw({ type: 'application/json' }), clerkWebHooks);
-
-module.exports =userRouter;
-
-=======
 const express = require('express');
 const { clerkWebHooks, sendSOS } = require('../controllers/userController');
 const authUser = require('../middleware/userAuth');
@@ -18,4 +8,3 @@ userRouter.post('/webhooks', clerkWebHooks);
 userRouter.post('/sos',authUser, sendSOS);
 
 module.exports =userRouter;
->>>>>>> 2f08537 (updating commit)
