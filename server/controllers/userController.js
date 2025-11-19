d50b4c52eb09e04da266448cf750a8fab46d8af6
Himanshu Verma <vhimanshu0817@gmail.com>
2025-11-20 00:23:42 +0530

    userController.js update

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -9,7 +9,7 @@ const clerkWebHooks = async (req, res) => {
     try {
         // create a svix instance with clerk webhook secret
         const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET);
-        await whook.verify(req.body, {
+        await whook.verify(JSON.stringify(req.body), {
             "svix-id": req.headers["svix-id"],
             "svix-timestamp": req.headers["svix-timestamp"],
             "svix-signature": req.headers["svix-signature"]
